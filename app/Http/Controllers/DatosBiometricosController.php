83a14c5e4e09d8f8cef955e7deb7c6aa0e4848e8
Gabriel <gabrielcorena@gmail.com>
2022-10-17 16:03:31 -0600

    guardando foto y firma

--- app/Http/Controllers/DatosBiometricosController.php
+++ app/Http/Controllers/DatosBiometricosController.php
@@ -2,11 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Biometrica;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage as FacadesStorage;
+use Storage;
 
 class DatosBiometricosController extends Controller
 {
     public function index(){
         return view('biometricos.index');
     }
+    public function store(Request $request)
+    {
+        //foto
+        $img = $request->image;
+        $folderPath = "fotos/";       
+        $image_parts = explode(";base64,", $img);
+        $image_type_aux = explode("image/", $image_parts[0]);
+        $image_type = $image_type_aux[1];
+        
+        $image_base64 = base64_decode($image_parts[1]);
+        $fileName = uniqid() . '.png';
+        
+        $file = $folderPath . $fileName;
+        Storage::put($file, $image_base64);
+        //guardando foto
+        $biometrico= new Biometrica();
+        $biometrico->foto_biometrica= $file;
+       // $biometrico->firma_biometrica= $file_f;
+        $biometrico->save();
+        // firma
+        $folderPath = 'firmas/';      
+        $image = explode(";base64,", $request->signed);
+        $image_type = explode("image/", $image[0]);
+        $image_type_png = $image_type[1];
+        $image_base64 = base64_decode($image[1]);
+        $file = $folderPath . uniqid() . '.'.$image_type_png;
+        
+        Storage::put($file, $image_base64);
+
+
+        // guardando firma
+        $biometrico->firma_biometrica= $file;
+        $biometrico->save();
+        
+        dd('Image uploaded successfully: '.$fileName . 'firma: '. $file);
+    }
+    
+
+
 }
--- resources/views/biometricos/index.blade.php
+++ resources/views/biometricos/index.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.plantilla')
+@section('css')
+<link rel="stylesheet" type="text/css" href="http://keith-wood.name/css/jquery.signature.css">
+<style>
+    .kbw-signature { width: 100%; height: 200px;}
+    #sig canvas{ width: 100% !important; height: auto;}
+</style>
+@endsection
 
 @section('contenido')
+
     <br>
     <div class="container">
         <div class="card">
@@ -11,9 +19,11 @@
            <div class="col-xs-12">
             <div class="col-md-12">
                 <h3> Paso 6</h3>
-  
+                
                 <div class="form-group">
                     <label for="description">Ingresar fotografia</label>
+                    <form method="POST" action="{{ route('biometricos.store') }}">
+                        @csrf
                     <div class="row">
                         <div class="col-md-6">
                             <div id="my_camera" style="height:auto;width:auto; text-align:left;"></div>
@@ -24,11 +34,29 @@
                         <div class="col-md-6">
                             <div id="results">Tu foto aparecerá aquí...</div>
                         </div>
-                        <div class="col-md-12 text-center">
+                        <br>
+                        <div class="card-header">
+                            <h5>Paso 7: ingresa tu firma</h5>
+                        </div>
+                        <div class="card-body">
+                        <div class="col-md-12">
+                            <label class="" for="">Dibuja tu firma:</label>
                             <br/>
-                            <button class="btn btn-primary nextBtn btn-lg pull-right" type="submit" >Siguiente</button>
+                            <div id="sig"></div>
+                            <br><br>
+                            <button id="clear" class="btn btn-danger">Borrar Firma</button>
+                            <textarea id="signature" name="signed" style="display: none"></textarea>
+                        </div>
+                        </div>
+
+
+                      <div class="col-md-12 text-center">
+                            <br/>
+
+                            <button class="btn btn-primary nextBtn btn-lg pull-right" type="submit" >Gurdar datos</button>
                         </div>
                     </div>
+                    </form>
                     <script language="JavaScript">
                         Webcam.set({
                             width: 490,
@@ -46,6 +74,7 @@
                             } );
                         }
                     </script>
+                   
                            
 
 
@@ -61,4 +90,21 @@
         </div>
             
     </div>
+
+
+     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> 
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="http://keith-wood.name/js/jquery.signature.js"></script>
+    <script type="text/javascript">
+        var sig = $('#sig').signature({syncField: '#signature', syncFormat: 'PNG'});
+        $('#clear').click(function(e) {
+            e.preventDefault();
+            sig.signature('clear');
+            $("#signature").val('');
+        });
+    </script>
+    
+
+
+
 @endsection    
